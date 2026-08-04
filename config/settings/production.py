786a70b5182178a546a73c55c7cb9307fbc0965e
EzequielHLC <hanzolovescoffee@gmail.com>
2026-08-04 16:39:51 -0300

    style: aplicar formateo de Black a archivos generados por Django

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,4 +1,4 @@
-from .base import * #noqa
+from .base import *  # noqa
 
 DEBUG = False
-ALLOWED_HOSTS = [] # completar con el dominio real cuando corresponda
+ALLOWED_HOSTS = []  # completar con el dominio real cuando corresponda
